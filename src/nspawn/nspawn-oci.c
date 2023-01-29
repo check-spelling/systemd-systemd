37cfb00656970d0e33f48683cc735ef8095c8a32
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-01-29 18:34:20 -0500

    spelling: cgroups
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/nspawn/nspawn-oci.c
+++ src/nspawn/nspawn-oci.c
@@ -49,7 +49,7 @@
  * should escaping be applied when joining mount options with ","?
  * devices cgroup support is bogus, "allow" and "deny" on the kernel level is about adding/removing entries, not about access
  * spec needs to say that "rwm" devices cgroup combination can't be the empty string
- * cgrouspv1 crap: kernel, kernelTCP, swappiness, disableOOMKiller, swap, devices, leafWeight
+ * cgroupsv1 crap: kernel, kernelTCP, swappiness, disableOOMKiller, swap, devices, leafWeight
  * general: it shouldn't leak lower level abstractions this obviously
  * unmanagable cgroups stuff: realtimeRuntime/realtimePeriod
  * needs to say what happense when some option is not specified, i.e. which defaults apply
